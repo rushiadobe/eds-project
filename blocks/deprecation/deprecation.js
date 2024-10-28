0f141b5443751496e231b1a554635153d1b09478
RUSHIKESH VAMAN NADAGUDE <433749@conneqtcorp.com>
2024-10-28 12:34:02 +0530

    Revert "added plugings and form css changes"
    This reverts commit 18cf8f8ab2aa1b872e45c134e8e53fcdb71405bb.

--- blocks/deprecation/deprecation.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  const header = document.createElement('div');
-  header.className = 'header';
-  header.textContent = 'Deprecation notice';
-  block.prepend(header);
-}
